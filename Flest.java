830a43198a7e33640d541993d5385dfff8742b0a
Mojjedrengen <morgan@famtorp.net>
2024-10-03 11:15:16 +0200

    Flest is Done

--- Flest.java
+++ Flest.java
@@ -12,27 +12,31 @@ public class Flest
         Map<String, Integer> notesMap = new HashMap<>();
         int highest = 0;
         for (int i = 0; i < n; i++) {
-            String in = s.nextLine().toLowerCase();
+            String[] in = s.nextLine().trim().toLowerCase().replaceAll("[,.!]+", "").split(" ");
+            Map<String, Integer> amount = new HashMap<>();
             for (String drikvare : drikkevarerArr){
-                if (in.contains(" "+drikvare+" ")||
-                    in.contains(" "+drikvare+".")||
-                    in.contains(" "+drikvare+",")||
-                    in.contains(" "+drikvare+"?")||
-                    in.contains(" "+drikvare+"!")||
-                    in.contains(" "+drikvare)    ||
-                    in.equals(drikvare)          ||
-                    in.contains(drikvare+" ")) {
-                    if (!notesMap.containsKey(drikvare)) {
-                        notesMap.put(drikvare, 1);
-                        if (highest == 0) highest = 1;
+                for (String word : in) {
+                    if (drikvare.equals(word)) {
+                        amount.put(word, amount.getOrDefault(word, 0)+1);
                     }
-                    else {
-                        int note = notesMap.get(drikvare);
-                        notesMap.put(drikvare, note+1);
-                        if (highest <= note+1) highest = note+1;
-                    }
-                    break;
-                } 
+                }
+            }
+            
+            String key = null;
+            for (String k : amount.keySet()) {
+                if (amount.size() == 0) break;
+                if (key == null) key = k;
+                if (amount.get(k) > amount.get(key)){
+                    key = k;
+                }
+            }
+            notesMap.put(key, notesMap.getOrDefault(key, 0)+1);
+            if (highest == 0) {
+             highest = 1;
+            } else if (key == null) {
+                continue;
+            } else if (notesMap.getOrDefault(key,0) > highest) {
+                highest = notesMap.get(key);
             }
         }
         final int high = highest;
@@ -46,8 +50,3 @@ public class Flest
         }
     }
 }
-/*Notes: 
-latte espresso latte
-Bliver til Latte
-Lav ekstra HashMap;
-*/
