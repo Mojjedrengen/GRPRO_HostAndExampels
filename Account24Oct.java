a379913f8f2f61e79e17b0f9f9d14b15402d425d
Mojjedrengen <morgan@famtorp.net>
2024-10-24 15:57:22 +0200

    oct 24

---
+++ Account24Oct.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Account24Oct here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Account24Oct
+{
+   String owner;
+   int balance;
+   
+   Account24Oct(String owner){
+       if (owner == null) throw new NullPointerException("owner is null");
+       this.owner = owner;
+       balance = 0;
+   }
+   
+   void deposit (int amount) throws IllegalArgumentException {
+       if (amount <= 0) throw new IllegalArgumentException("Amount can't be negative");
+       balance += amount;
+   }
+   
+   void withdraw(int amount) {
+       balance -= amount;
+   }
+   
+   void inspect(){
+       System.out.print("balance: "+ balance+"\n");
+   }
+}
---
+++ oct24.java
@@ -0,0 +1,67 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Write a description of class oct24 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class oct24
+{
+    public static void nullPoint(){
+        List<Integer> list = new ArrayList<>();
+        list.add(null);
+        System.out.println(list.get(0)+1);
+    }
+    
+    public static void outOfBounds(){
+        int[] arr = {1};
+        System.out.println(arr[1]);
+    }
+    
+    public static void arithmetic(){
+        System.out.println(1/0);
+    }
+    
+    public static void classCast(){
+        List<Integer> list = new ArrayList<>();
+        System.out.println((HashMap)list);
+    }
+    
+    public static void numberFormat(){
+        String x = "Hej";
+        System.out.println(Integer.parseInt(x)+1);
+    }
+    
+    
+    
+    private static void makeAccount() {
+        Account24Oct acount = new Account24Oct(null);
+    }
+    
+    public static void testAccount() {
+        try {
+            makeAccount();
+        } catch (NullPointerException e) {
+            System.out.println(e);
+        }
+    }
+    
+    private static void makeDeposit(Account24Oct a, int val) throws IllegalArgumentException {
+        a.deposit(val);
+    }
+    
+    public static void testAccount2() {
+        int val = -5;
+        Account24Oct acount = new Account24Oct("Hej");
+        try {
+            makeDeposit(acount, val);
+        } catch (IllegalArgumentException e) {
+            System.out.print("error: ");
+            System.out.println(e.getMessage());
+            System.out.println("Please enter legal amount");
+        }
+    }
+}
